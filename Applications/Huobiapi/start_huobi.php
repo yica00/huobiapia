a3d9cf441931a0cd44aa1324358b3b8b183e682e
yica00 <Licheng1028>
2019-11-17 23:34:09 +0800

    只输出最低和最高价

--- Applications/Huobiapi/start_huobi.php
+++ Applications/Huobiapi/start_huobi.php
@@ -89,7 +89,9 @@ $worker->onWorkerStart = function($worker)
 
             $msg=[];
 
-            file_put_contents("./deta.txt",var_export($data,true)."%%-----------\n",FILE_APPEND);
+            if( isset( $data['tick'] ) && isset( $data['tick']['low'])    && isset( $data['tick']['high'] )  ){
+                file_put_contents("./deta.txt","low:{$data['tick']['low']}---hight:{$data['tick']['high'] }--"."%%-----------\n",FILE_APPEND);
+            }
 
 //            $hbrds= new HuobiRedis("127.0.0.1",6379);
 //            if(isset($data['ch'])) {
