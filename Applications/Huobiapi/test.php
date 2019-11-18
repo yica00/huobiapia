a9c732c4b408aa29ba4d8bd9374d380ea3605275
licheng <licheng1028@126.com>
2019-11-18 21:54:31 +0800

    php获取redis

--- Applications/Huobiapi/test.php
+++ Applications/Huobiapi/test.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/Notify.php';
 $notify = new Notify();
 
 while (1){
-    echo $notify->redis->get( 'low' ) . "__". $notify->redis->get( 'hight') .'\r\n';
+    echo $notify->redis->get( 'low' ) . "__". $notify->redis->get( 'hight') ."\r\n";
     sleep(1);
 }
